bdd533ece03902ee107b348c7e7ba69ff53110d9
azk <azk@mac.lan>
2022-09-25 10:28:14 +0000

    Fix command not working

--- src/Controller/UtilsController.php
+++ src/Controller/UtilsController.php
@@ -2,23 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Transaction;
-use App\Entity\User;
-use App\Repository\TontineRepository;
-use App\Repository\TransactionRepository;
-use App\Service\Transaction\GetUserTransactions;
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
 
 #[Route('/api/utils')]
 class UtilsController extends AbstractController
@@ -26,8 +16,19 @@ class UtilsController extends AbstractController
     //Validate Transactions
     #[Route('/validate-transactions', name: 'validate_transactions', methods: ['GET'])]
     public function validateTransactions(
+        KernelInterface $kernel
     ): JsonResponse {
-        shell_exec('php bin/console mtn:validate');;
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $input = new ArrayInput([
+            'command' => 'mtn:validate',
+        ]);
+
+        // You can use NullOutput() if you don't need the output
+        $output = new NullOutput();
+        $application->run($input, $output);
+
         return $this->json(
             [
                 'message' => 'Transactions validated',
